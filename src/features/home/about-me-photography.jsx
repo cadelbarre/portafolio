abe1379ec28758287cabb55b55349b2d3fe487b2
Carlos Delbarre <cadelbarre@gmail.com>
2023-11-14 20:09:45 -0500

    Change extension

--- src/features/home/about-me-photography.jsx
+++ src/features/home/about-me-photography.jsx
@@ -1,11 +1,13 @@
-import { Fancybox } from "@fancyapps/ui";
+import * as Fancyapps from "@fancyapps/ui";
+const Fancybox = Fancyapps.Fancybox;
+
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
 const photos = ['cocodrilo', 'garza', 'patitos', 'gokarts', 'portrait', 'pregnancy']
 
 export default function AboutMePhotography() {
 
-  Fancybox.bind('[data-fancybox="gallery"]', {})
+  Fancybox?.bind('[data-fancybox="gallery"]', {})
 
   return (
     <section className="grid grid-cols-2 md:grid-cols-3 gap-4">
